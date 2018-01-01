cbee106d0f79dfea294dd7284ae88bca00255c79
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-01-01 13:58:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import config
 import telebot
 from bot import bot
 from flask import Flask, request
-from multiprocessing import Process
 
 bot.remove_webhook()
 bot.set_webhook(f'https://<app_name>.herokuapp.com/{config.token}')
 server = Flask(__name__)
 
-@server.route("/keep_alive", methods=['POST'])
+@server.route("/", methods=['GET'])
 def keepAlive():
     # Just to not let the server on the free Heroku account go to sleep
+    print('Keep alive: OK.')
     return '!', 200
 
 @server.route(f"/{config.token}", methods=['POST'])
@@ -20,5 +20,4 @@ def getMessage():
     return "!", 200
 
 if __name__ == '__main__':
-    Process(target=app_ping.main).start()
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', '5000')))
